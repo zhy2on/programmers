620c49c83a1da3a24547487e71ce5ff7720f0326
zhy2on <dhwlgus5940@gmail.com>
2021-01-15 15:30:23 +0900

    올바른 괄호

--- level2/correctBracket.c
+++ level2/correctBracket.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-void push(char* stack, int* top, char c){
-	stack[++(*top)] = c;
-}
 bool solution(const char* s) {
 	int top = -1;
 	char* stack = (char*)malloc((strlen(s)+1) * sizeof(char));
 
 	while(*s){
-		if(*s == '(') push(stack, &top, *s);
+		if(*s == '(') stack[++top] = *s;
 		else {
 			if(top == -1) return false;
 			else top--;
