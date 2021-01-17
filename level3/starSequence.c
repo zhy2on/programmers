0c49b9e874890886c33fca05520617123ee48ad5
zhy2on <dhwlgus5940@gmail.com>
2021-01-17 20:05:42 +0900

    스타수열

--- level3/starSequence.c
+++ level3/starSequence.c
@@ -1,66 +1,57 @@
-//스타수열 ver2 연결리스트 구현 해봤던 거 일단 저장
+//스타수열 ver3 연결리스트 없이 -> 테스트케이스 오류 2, 14, 15, 28
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node {
-	int idx;
-	typedef struct node* next;
-}Node;
 typedef struct star {
-	int len, flag;
-	Node* head;
-	Node* last;
+	int len, flag, bfIdx;
 }Star;
-void initStar(Star* star, int len);
-Node* getNode(int idx);
-void addNodeAfter(Star* star, int idx);
-void updateLen(Node* before, int* len, int* flag);
+void initStar(Star* star, int a_len);
+void updateLen(Star* star, int curIdx);
+void lastUpdate(Star* star, int curIdx);
 int solution(int a[], int a_len);	
 int main() {
-	int a[] = { 5, 2, 3, 3, 5, 3 };
+	int a[] = { 3,3,2,2,3,3 };
 	int a_len = 6;
 	
-	printf("%d\n", solution(a[], a_len));
+	printf("%d\n", solution(a, a_len));
 
 	return 0;
 }
-void initStar(Star* star, int len) {
-	Star* p;
-	for(int i = 0; i < len; i++) {
-		p = star + i;
-		p->len = 0;
-		p->idx = 1;
-		p->last = p->head = NULL;
+void initStar(Star* star, int a_len) {
+	for(int i = 0; i < a_len; i++) {
+		(star + i)->len = 0; 
 	}
 }
-Node* getNode(int idx) {
-	Node* p;
-	p = (Node*)malloc(sizeof(Node));
-	p->idx = idx;
-	p->next = NULL;
-	return p;
+void updateLen(Star* star, int curIdx) {
+	int bfIdx = star->bfIdx;
+
+	if(star->len == 0) { //처음 업데이트 되는 경우일 때
+		if(curIdx > 0) { star->flag = -1; star->len += 2; }
+		else { star->flag = 1; star->len += 2; }
+		star->bfIdx = curIdx;
+		return;
+	}
+	
+	if(star->flag == -1) { //이전 인덱스가 왼쪽과 묶인 경우
+		if(curIdx - bfIdx > 1) { star->flag = -1; star->len += 2; } //왼쪽에 자리가 있는 경우
+		else { star->flag = 1; star->len += 2; } //왼쪽에 자리가 없는 경우
+	}
+
+	else { //if(star->flag == 1) //이전 인덱스가 오른쪽과 묶인 경우
+		if(curIdx == bfIdx + 1) { star->flag = -1; } //현재 인덱스가 이전 인덱스의 오른쪽인 경우
+		else if(curIdx - (bfIdx + 1) > 1) { star->flag = -1; star->len += 2; } //왼쪽에 자리가 있는 경우
+		else { star->flag = 1; star->len += 2; } //왼쪽에 자리가 없는 경우
+	}
+
+	star->bfIdx = curIdx;
 }
-void addNodeAfter(Star* star, int idx) {
-	if(star->head == NULL) {
-		star->last = star->head = getNode(idx);
+void lastUpdate(Star* star, int curIdx) { //마지막 원소인 경우 해당 스타배열 기준으로 왼쪽 자리가 남아있는 경우만 len갱신
+	int bfIdx = star->bfIdx;
+
+	if(star->flag == -1) {
+		if(curIdx - bfIdx > 1) star->len += 2;
 	}
 	else {
-		star->last->next = getNode(idx);
-		updateLen(star->last, &(star->len), &(star->flag));
-		star->last = star->last->next;
-	}
-}
-void updateLen(Node* before, int* len, int* flag) {
-	int bfIdx = before->idx, curIdx = before->next->idx;
-
-	if(*flag == -1) { //이전 인덱스가 왼쪽과 묶인 경우
-		if(curIdx - bfIdx > 1) { *flag = -1; *len += 2; } //왼쪽에 자리가 있는 경우
-		else { *flag = 1; *len += 2; } //왼쪽에 자리가 없는 경우
-	}
-
-	else { //if(*flag == 1) //이전 인덱스가 오른쪽과 묶인 경우
-		if(curIdx == bfIdx + 1) { *flag = -1; } //현재 인덱스가 이전 인덱스의 오른쪽인 경우
-		else if(curIdx - (bfIdx + 1) > 1) { *flag = -1; *len += 2; } //왼쪽에 자리가 있는 경우
-		else { *flag = 1; *len += 2; } //왼쪽에 자리가 없는 경우
+		if(curIdx - (bfIdx + 1) > 1) star->len += 2;
 	}
 }
 int solution(int a[], int a_len) {
@@ -74,9 +65,10 @@ int solution(int a[], int a_len) {
 	initStar(star, a_len);
 
 	//2. a[]돌면서 star[]채우기 이 때 각 리스트의 최대 스타수열 길이 len을 저장
-	for(i = 0; i < a_len; i++){
-		addNodeAfter(star + a[i]);
+	for(i = 0; i < a_len - 1; i++){
+		updateLen(star+a[i], i); 
 	}
+	lastUpdate(star+a[i], i);
 
 	//3. star[] 돌면서 최대길이 max찾기
 	max = star[0].len;
