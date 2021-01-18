4d01e34439c6ee4f9823064c01a320efc28a60a1
zhy2on <dhwlgus5940@gmail.com>
2021-01-19 01:12:40 +0900

    풍선 터트리기

--- level3/poppingBalloon.c
+++ level3/poppingBalloon.c
@@ -1,25 +1,11 @@
-//ver1. 시간초과
+//ver3
+typedef struct sol{
+	int* a;
+	int* b;
+}Sol;
 #include <stdio.h>
 #include <stdlib.h>
-// a_len은 배열 a의 길이입니다.
-int solution(int a[], int a_len) { 
-	int answer = 2, i, j;
-	//왼쪽, 오른쪽에서 기준 원소보다 작은 원소가 존재하지 않는 곳이 한 쪽이라도 있으면 true
-	for(i = 1; i <  a_len - 1; i++) { 
-		for(j = 0; j < i; j++) { //왼쪽 비교
-			if(a[j] < a[i]) break;
-		}
-		if(j == i) {
-			answer++; continue;
-		}
-		for(j = i + 1; j < a_len; j++) { //오른쪽 비교
-			if(a[j] < a[i]) break;
-		}
-		if(j == a_len) answer++;                
-	}
-
-	return answer;
-}
+int solution(int a[], int a_len);
 int main(){
 	int a[] = {-16, 27, 65, -2, 58, -92, -71, -68, -61, -33};
 	int a_len = 10;
@@ -27,4 +13,21 @@ int main(){
 	printf("%d\n", solution(a, a_len));
 
 	return 0;
+}
+int compare(const void* a, const void* b) {
+
+}
+void initSol(Sol* sol, int a[], int a_len) {
+	sol->a = (int*)malloc(a_len * sizeof(int));
+	sol->b = (int*)malloc(a_len * sizeof(int));
+	for(int i = 0; i < a_len; i++) {
+		sol->a[i] = a[i];
+		sol->b[i] = i;
+	}
+}
+int solution(int a[], int a_len) {
+	Sol sol;
+	initSol(&sol, a, a_len);
+
+
 }
